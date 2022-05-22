0edf306e5cec9038f68a4395b183a135c8c25e24
Streamer devs <106023124+Streamer-devs@users.noreply.github.com>
2022-05-22 10:57:52 -0400

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "ODg4MTg5MzU3MzU2MzcyMDg4.YUPEzA.537BvkY3tTyMOw7gENnLA5c-Zxw",
+    prefix: ">>",
+    admins: [
+        "684544746613506077"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
